3ec7a50fa03963e9cd6c226c60370eca07e8a002
Ethbian <ethbian@protonmail.com>
2020-04-22 00:09:05 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # jitsi-meet-stats
 Jitsi Meet stats to Graphite exporter
+
+## Introduction
+Jitsi (Videobridge) provides a lot of useful [statistics](https://github.com/jitsi/jitsi-videobridge/blob/master/doc/statistics.md).  
+Graphite can be used to store them, Grafana to show them in a cool way.  
+
+## Installation
+
+Created and tested on Ubuntu 18 with Python 2.7
+
+### (1/3) enabling Videobrige statistics reporting
+
+In the **/etc/jitsi/videobridge/** directory two files need to be updated.  
+
+**config** file - replace the empty JVB_OPTS with the following:  
+
+```
+# extra options to pass to the JVB daemon
+#JVB_OPTS=""
+JVB_OPTS="--apis=rest,xmpp"
+```
+
+**sip-communicator.properties** file - add the following lines:
+
+```
+org.jitsi.videobridge.ENABLE_STATISTICS=true
+org.jitsi.videobridge.STATISTICS_TRANSPORT=muc,colibri
+org.jitsi.videobridge.STATISTICS_INTERVAL=5000
+```
+
+The last one defines how often the stats should be updated (here: 5 seconds).  
+Now - restart the **videobridge** service and check with **curl** if the stats are there:
+
+> curl http://localhost:8080/colibri/stats
+
+### (2/3) install jmstats service
+
+to be continued
+
+
+
+
+
---
+++ jmstats.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+#
+# Jitsi Meet to Graphite exporter
+# version 0.1
+# for latest version, howto, grafana dashboard visit
+# https://github......
+#
+
+import requests
+import json
+import time
+import pickle
+import struct
+import socket
+import logging
+
+# change these:
+CARBON_SERVER = 'graphite.example.com'
+GRAPHITE_PREFIX = 'metrics.jitsi.{}'.format(socket.gethostname())
+LOG_FILE = '/var/log/jitsi/jmstats.log'
+SKIP_METRICS = ['current_timestamp', 'conference_sizes']
+JITSI_STATS = 'http://localhost:8080/colibri/stats'
+CARBON_PICKLE_PORT = 2004
+SLEEP_SEC = 5
+
+
+def get_stats():
+    err = False
+    result = {}
+    try:
+        r = requests.get(JITSI_STATS)
+    except Exception as ex:
+        logging.error('Error getting jitsi stats: {}'.format(ex))
+        err = True
+    if not err:
+        try:
+            result = r.json()
+        except Exception as ex:
+            logging.error('Error converting json to dict: {}'.format(ex))
+            err = True
+    return err, result
+
+
+def stats2graphite(stats):
+    metrics = ([])
+    now = int(time.time())
+    for key in stats.keys():
+        if key in SKIP_METRICS:
+            continue
+        metric = '{}.{}'.format(GRAPHITE_PREFIX, key)
+        metrics.append((metric, (now, stats[key])))
+    payload = pickle.dumps(metrics, protocol=2)
+    header = struct.pack("!L", len(payload))
+    package = header + payload
+    return package
+
+
+logging.basicConfig(filename=LOG_FILE, filemode='a', level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler())
+clientSocket = socket.socket()
+clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    clientSocket.connect((CARBON_SERVER, CARBON_PICKLE_PORT))
+except Exception as ex:
+    logging.error('Error creating socket: {}'.format(ex))
+    raise SystemExit()
+connected = True
+
+while True:
+    err, stats = get_stats()
+    if not err:
+        stats2send = stats2graphite(stats)
+        try:
+            logging.info('{} sending jitsi stats...'.format(time.ctime()))
+            clientSocket.sendall(stats2send)
+        except socket.error:
+            connected = False
+            clientSocket = socket.socket()
+            logging.warning('Connection lost... ')
+            while not connected:
+                try:
+                    clientSocket.connect((CARBON_SERVER, CARBON_PICKLE_PORT))
+                    connected = True
+                    logging.warning('re-connection successful')
+                except socket.error:
+                    logging.warning('{} reconnecting...'.format(time.ctime()))
+                    time.sleep(SLEEP_SEC)
+    time.sleep(SLEEP_SEC)
+
+clientSocket.close()
